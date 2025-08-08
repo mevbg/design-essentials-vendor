859265a733ba5b71779c62ddd4d85bfba3e85b78
Martin Metodiev <m@mev.bg>
2025-08-08 13:17:09 +0300

    Remove TODO comment

--- src/types/generator.types.ts
+++ src/types/generator.types.ts
@@ -8,5 +8,4 @@ import type StyleDictionary from 'style-dictionary';
 // ---------------------------------------------------
 // GENERATOR FUNCTION
 
-// TODO
 export type GeneratorFn<T, Response = StyleDictionary> = (params: T) => Promise<Response>;
